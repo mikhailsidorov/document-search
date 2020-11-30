4f3d395175eaf07c8188f6d681a227cc2f44458a
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2020-11-30 17:06:14 +0300

    Fix get documents API

--- backend/backend/documents/crud.py
+++ backend/backend/documents/crud.py
@@ -1,7 +1,8 @@
-from typing import List, Optional
+from typing import List, Optional, Literal
 from datetime import datetime
 
 from sqlalchemy.orm import Session
+
 
 from . import models, schemas
 
@@ -10,14 +11,28 @@ def get_document(db: Session, document_id: int) -> Optional[models.Document]:
     return db.query(models.Document).filter(models.Document.id == document_id).first()
 
 
-def get_documents(db: Session, offset: int = 0, limit: int = 100,
-                  date_start: datetime = None, date_end: datetime = None) -> List[models.Document]:
+def get_documents(db: Session,
+                  name: Optional[str],
+                  order_by: Literal['asc', 'desc'],
+                  sort_by: Literal['id', 'created_at', 'name'],
+                  offset: int = 0,
+                  limit: int = 100,
+                  date_start: datetime = None,
+                  date_end: datetime = None) -> List[models.Document]:
+
     query = db.query(models.Document)
     if date_start:
         query = query.filter(models.Document.created_at >= date_start)
 
     if date_end:
         query = query.filter(models.Document.created_at <= date_end)
+
+    if name:
+        print(name)
+        search = "%{}%".format(name)
+        query = query.filter(models.Document.name.like(search))
+
+    query = query.order_by(getattr(getattr(models.Document, sort_by), order_by)())
 
     return query.offset(offset).limit(limit).all()
 
