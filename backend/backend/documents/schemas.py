dcdb79905a0713e8b99bb998742e076d8d999e71
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2020-11-30 09:37:20 +0300

    Add camel-case serialization support

--- backend/backend/documents/schemas.py
+++ backend/backend/documents/schemas.py
@@ -1,12 +1,27 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from humps import camelize
 
 
-class DocumentBase(BaseModel):
-    name: str
+def to_camel_case(string: str) -> str:
+    return camelize(string)
+
+
+class CamelModel(BaseModel):
+    class Config:
+        alias_generator = to_camel_case
+        allow_population_by_field_name = True
+
+
+class DocumentBase(CamelModel):
+    name: str = Field()
     description: Optional[str] = None
+
+    class Config():
+        alias_generator = to_camel_case
+        allow_population_by_field_name = True
 
 
 class DocumentCreate(DocumentBase):
