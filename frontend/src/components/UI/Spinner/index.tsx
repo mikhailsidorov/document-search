3aa9f8761c41641a4128f449567207ea0fdc9ee4
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2020-11-30 17:02:46 +0300

    Fix spinner

--- frontend/src/components/UI/Spinner/index.tsx
+++ frontend/src/components/UI/Spinner/index.tsx
@@ -3,5 +3,5 @@ import { FC } from 'react';
 import styles from './Spinner.module.scss';
 
 export const Spinner: FC = () => {
-  return <div className={styles.spinner}>...Загрузка</div>;
+  return <div className={styles.loader}>...Загрузка</div>;
 };
